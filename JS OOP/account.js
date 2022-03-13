1812116bb57062880d3bf166ea08d3c02311993d
Sans <hewhototallymustnotbenamed@gmail.com>
2022-03-13 19:50:55 +0530

    account

---
+++ JS OOP/account.js
@@ -0,0 +1,27 @@
+class Account{
+    constructor(name) {
+        this.name = name;
+        this.balance = 0;
+    }
+    credit(val) {
+        this.balance += val; 
+        return val;
+    }
+    describe() {
+        return `Name: ${this.name} \nbalance: ${this.balance}`;
+    }
+}
+
+const poor = new Account("poor");
+const rich = new Account("rich")
+console.log(poor.balance);
+console.log(rich.balance);
+
+poor.credit(10);
+rich.credit(999999);
+
+console.log(poor.balance);
+console.log(rich.balance);
+
+console.log(poor.describe());
+console.log(rich.describe());
